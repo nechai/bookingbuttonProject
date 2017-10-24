e4a3b780c87ecc78e4a7f1b4eb8ce3c4b5d6cfc5
nechai <nviwork7@gmail.com>
2017-10-24 14:55:41 +0300

    add style for popup, datepicker fields

--- themes/custom/bookbtn/js/common.js
+++ themes/custom/bookbtn/js/common.js
@@ -103,9 +103,78 @@
                     cssEase: 'linear',
                 });
             });
-
         }
-    }
+    };
+
+    // Drupal.behaviors.lightsliderBehavior = {
+    //     attach: function (context, settings) {
+    //
+    //         // Using once() with more complexity.
+    //         $.once().each(function () {
+    //             $('.popup-block .field--name-field-image').lightSlider({
+    //
+    //                 item: 1,
+    //                 slideMargin: 0,
+    //
+    //                 autoWidth: true,
+    //                 // slideMove: 1, // slidemove will be 1 if loop is true
+    //                 // slideMargin: 10,
+    //                 //
+    //                 // // addClass: '',
+    //                 mode: "fade",
+    //                 useCSS: true,
+    //                 // cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+    //                 // easing: 'linear', //'for jquery animation',////
+    //                 //
+    //                 // speed: 400, //ms'
+    //                 // auto: false,
+    //                 // pauseOnHover: false,
+    //                 // loop: false,
+    //                 // slideEndAnimation: false,
+    //                 // pause: 2000,
+    //                 //
+    //                 // keyPress: false,
+    //                 // controls: true,
+    //                 // prevHtml: '',
+    //                 // nextHtml: '',
+    //                 //
+    //                 // rtl: false,
+    //                 // adaptiveHeight: false,
+    //                 //
+    //                 // vertical: false,
+    //                 verticalHeight: 500,
+    //                 vThumbWidth: 100,
+    //                 //
+    //                 thumbItem: 5,
+    //                 // // pager: true,
+    //                 gallery: true,
+    //                 galleryMargin: 5,
+    //                 thumbMargin: 5,
+    //                 // currentPagerPosition: 'middle',
+    //                 //
+    //                 // // enableTouch:true,
+    //                 // // enableDrag:true,
+    //                 // freeMove: true,
+    //                 // swipeThreshold: 40,
+    //                 //
+    //                 // responsive: [],
+    //                 //
+    //                 // onBeforeStart: function (el) {
+    //                 // },
+    //                 // onSliderLoad: function (el) {
+    //                 // },
+    //                 // onBeforeSlide: function (el) {
+    //                 // },
+    //                 // onAfterSlide: function (el) {
+    //                 // },
+    //                 // onBeforeNextSlide: function (el) {
+    //                 // },
+    //                 // onBeforePrevSlide: function (el) {
+    //                 // }
+    //             });
+    //         })
+    //     }
+    // };
 
 
 
@@ -122,5 +191,108 @@
             });
         }
     };
+
+
+    Drupal.behaviors.datapickerBehavior = {
+        attach: function (context, settings) {
+
+
+            $(function () {
+                // $('#edit-field-booking-dates-value').datepicker();
+                // $('#edit-field-booking-dates-end-value').datepicker();
+                var currDate = new Date();
+                var dateFormat = "yy-mm-dd";
+                    from = $( "#edit-field-booking-dates-value" )
+                        .datepicker({
+                            minDate: currDate,
+                            dateFormat: dateFormat,
+                            defaultDate: new Date(),
+                            changeMonth: true,
+                            numberOfMonths: 2
+                        }).datepicker("setDate", currDate)
+                        .on( "change", function() {
+                            to.datepicker( "option", "minDate", getDate( this ) );
+                        }),
+                    to = $( "#edit-field-booking-dates-end-value" ).datepicker({
+                        minDate: currDate,
+                        dateFormat: dateFormat,
+                        defaultDate: "+1w",
+                        changeMonth: true,
+                        numberOfMonths: 2
+                    }).datepicker("setDate", "+1d")
+                        .on( "change", function() {
+                            from.datepicker( "option", "maxDate", getDate( this ) );
+                        });
+
+                function getDate( element ) {
+                    var date;
+                    try {
+                        date = $.datepicker.parseDate( dateFormat, element.value );
+                    } catch( error ) {
+                        date = null;
+                    }
+
+                    return date;
+                }
+
+            });
+
+        }
+    };
+
+    // Drupal.behaviors.airdatepickerBehavior = {
+    //     attach: function (context, settings) {
+    //
+    //         // Using once() with more complexity.
+    //         $(function () {
+    //             $('#edit-field-booking-dates-value').datepicker({
+    //                 dateFormat: "yyyy-mm-dd",
+    //                 minDate: new Date(),
+    //                 numberOfMonths: 2,
+    //
+    //                 // // auto submit
+    //                 // onSelect: function() {
+    //                 //     $("#views-exposed-form-apartment-page-1").submit();
+    //                 // }
+    //             });
+    //             $('#edit-field-booking-dates-end-value').datepicker({
+    //                 dateFormat: "yyyy-mm-dd",
+    //             });
+    //         });
+    //     }
+    // };
+
+    Drupal.behaviors.imageBoxBehavior = {
+        attach: function (context, settings) {
+
+            // Using once() with more complexity.
+            $(function () {
+                $('.field--name-field-image .field__item').click(function () {
+                    if (!$(this).hasClass('image-box') && !$('.field--name-field-image .field__item').hasClass('image-box')) {
+                        $(this).addClass('image-box');
+                    } else if (!$(this).hasClass('image-box') && $('.field--name-field-image .field__item').hasClass('image-box')) {
+                        $('.field--name-field-image .field__item').removeClass('image-box');
+                        $(this).addClass('image-box');
+                    } else {
+                        //do nothing
+                    }
+                })
+            });
+        }
+    };
+
+    // Drupal.behaviors.authoSubmitBehavior = {
+    //     attach: function (context, settings) {
+    //
+    //         // Using once() with more complexity.
+    //         $(function () {
+    //             // Autosubmit 'view-class-here' views exposed form.
+    //             $("div.views-exposed-form").find("#views-exposed-form-apartment-page-1").find("select").bind("change", function () {
+    //                 $(this).closest("form").trigger("submit");
+    //             }).end().find("input[type='submit']").addClass("visually-hidden");
+    //         });
+    //     }
+    // };
+
 
 })(jQuery);
